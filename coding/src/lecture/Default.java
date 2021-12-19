d45eb887b95441e3528a4598dbc667403a2ba2c5
minsoo.lee <letomok@gmail.com>
2021-12-19 16:22:04 +0900

    문자열 - 10번

---
+++ coding/src/lecture/Default.java
@@ -0,0 +1,18 @@
+package lecture;
+
+import java.util.Scanner;
+// 제출 시, Default -> Main 으로 클래스이름 변경해야함
+public class Default {
+    public int solution(String str, char t) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Default T = new Default();
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println();
+    }
+}
