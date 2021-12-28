dd73d2c6d610413d466e314f71c9add0705d32df
minsoo.lee <letomok@gmail.com>
2021-12-28 14:52:50 +0900

    연속된 자연수의 합, 최대 길이 연속부분수열(★★★) 추가

--- coding/src/lecture/Default.java
+++ coding/src/lecture/Default.java
@@ -3,16 +3,12 @@ package lecture;
 import java.util.Scanner;
 // 제출 시, Default -> Main 으로 클래스이름 변경해야함
 public class Default {
-    public int solution(String str, char t) {
-        int answer = 0;
+    public void solution() {
 
-        return answer;
     }
 
     public static void main(String[] args) {
-        Default T = new Default();
-        Scanner kb = new Scanner(System.in);
 
-        System.out.println();
+        Scanner in = new Scanner(System.in);
     }
 }
