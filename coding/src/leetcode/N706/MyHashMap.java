45d0b8bf7e34acb2541f5c53d49403e3e5eb2bca
minsoo.lee <letomok@gmail.com>
2022-04-23 20:44:44 +0900

    백준 1991

--- coding/src/leetcode/N706/MyHashMap.java
+++ coding/src/leetcode/N706/MyHashMap.java
@@ -4,12 +4,18 @@ import java.util.Arrays;
 
 public class MyHashMap {
 
-    private int[] store =  new int[1000001];
+    private int[] store =  new int[10];
     public MyHashMap() {
         Arrays.fill(store, -1);
     }
 
     public void put(int key, int value) {
+        if (store.length < key) {
+            int[] tmp = new int[key+5];
+            System.arraycopy(store, 0, tmp, 0, store.length);
+            System.arraycopy(tmp, 0, store, 0, tmp.length);
+        }
+
         store[key] = value;
     }
 
@@ -22,7 +28,6 @@ public class MyHashMap {
     }
 
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
         MyHashMap myHashMap = new MyHashMap();
         myHashMap.put(1, 1); // The map is now [[1,1]]
         myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
@@ -32,7 +37,6 @@ public class MyHashMap {
         myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
         myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
         myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
-        long end = System.currentTimeMillis();
-        System.out.println(end - start);
+        System.out.println();
     }
 }
